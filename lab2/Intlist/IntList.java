9fd694464562c8e759a93efb3e5e2cd5faf711a3
Diyang <Diyang@ufl.edu>
2021-12-28 16:05:50 -0500

    IntList commit_2 Modify null argument

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -97,6 +97,9 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if (A == null){
+            return B;
+        }
         IntList newList = new IntList(A.first, null);
         IntList p = newList;
         A = A.rest;
